a94b6fe6d183bab0f1e8f31d50d9013258372b6f
George <40739548+duttongames@users.noreply.github.com>
2022-09-12 18:37:48 +0100

    Basic Player and Camera Movement

---
+++ Takeover2022/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    //Variables
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private Vector3 offset;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, transform.position.y + offset.y, transform.position.z + offset.z);
+    }
+}
