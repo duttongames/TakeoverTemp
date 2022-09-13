4fcf6ff63d84c7c52f97453fc43d1e5eed46b3c8
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-09-13 13:42:08 +0100

    Update MeshGen.cs

--- Takeover2022/Assets/Scripts/MeshGen.cs
+++ Takeover2022/Assets/Scripts/MeshGen.cs
@@ -1,30 +1,34 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace MeshGen
 {
+    [RequireComponent(typeof(MeshFilter))]
     public class MeshGen : MonoBehaviour
     {
-        static void Awake()
+        double NEG_X, NEG_Y = -1;
+        double POS_X, POS_Y = 1;
+
+        static void Start()
         {
-            Mesh MESH = new Mesh();
-            MESH.name = "Proc";
+            CREATE_SHAPE();
+        }
+
+        static void CREATE_SHAPE()
+        {
+            Vector3[] Vertices = new Vector3[]
+            {
+                new Vector3(0,0,0),
+                new Vector3(0,0,1),
+                new Vector3(1,0,0)
+            };
 
             int[] TRI = new int[]
             {
                 1,0,2,
                 3,1,2
             };
-
-            MESH.triangles = TRI;
-            return;
-        }
-
-        static void SET_VERTS()
-        {
-            double NEG_X, NEG_Y = -1;
-            double POS_X, POS_Y = 1;
         }
     }
 }
