b023237bd813fde67974dc71174cd168ab95971b
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-09-14 10:01:54 +0100

    Update MeshGen.cs

--- Takeover2022/Assets/Scripts/MeshGen.cs
+++ Takeover2022/Assets/Scripts/MeshGen.cs
@@ -7,6 +7,7 @@ namespace MeshGen
     [RequireComponent(typeof(MeshFilter))]
     public class MeshGen : MonoBehaviour
     {
+        // DOUBLE VALUE NOTATION TO REPRESENT POINTS //
         private double NEG_X, NEG_Y = -1;
         private double POS_X, POS_Y = 1;
 
@@ -15,8 +16,11 @@ namespace MeshGen
             CREATE_SHAPE();
         }
 
+        // THE MAIN FUNCTION FOR MESH GENERATION //
         static void CREATE_SHAPE()
         {
+            // USING VECTOR3 NOTATION TO DESCERN ALL 3 AXES //
+            // THEIR POINTS ARE SCAILABLE AND CAN BE ADJUSTED RELATIVELY // 
             Vector3[] Vertices = new Vector3[]
             {
                 new Vector3(0,0,0),
@@ -24,6 +28,7 @@ namespace MeshGen
                 new Vector3(1,0,0)
             };
 
+            // INT ARRAY TO INITIALISE TRIANGULATION //
             int[] TRI = new int[]
             {
                 1,0,2,
